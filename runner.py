5c4d3c73dc6e3150122468a47160352ddaa9d769
Vaibhav Nitnaware <vaibhavnit007@gmail.com>
2022-12-07 14:58:30 -0600

    minor changes

--- runner.py
+++ runner.py
@@ -16,7 +16,7 @@ from get_parameter_number import get_parameter_number
 
 doc_dir = "./publaynet/"
 result_dir = "./results/"
-weights = "./pretrained_models/deeprft_deblurring.pth"
+weights = "./DeepRFT-MIMO-v1/DeepRFT_Small/model_GoPro.pth"
 
 num_res = 8         # num of resblocks, [Small, Med, PLus]=[4, 8, 20]
 win = 256           # window size, [GoPro, HIDE, RealBlur]=256, [DPDD]=512
